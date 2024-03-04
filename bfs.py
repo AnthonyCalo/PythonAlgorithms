6bca2c59afb6442db50fe4e3c6a1440c2870f54d
Anthony Calo <acalo4554@gmail.com>
2024-03-03 20:30:26 -0500

    test

--- bfs.py
+++ bfs.py
@@ -1,70 +1,87 @@
-class Vertex:
-    def __init__(self, n):
-        self.name=n
-        self.neighbors=[]
 
-        self.distance=9999
-        self.visited=False
-    def add_neighbor(self,v):
-        if v not in self.neighbors:
-            self.neighbors.append(v)
-            self.neighbors.sort()
-        else:
-            return
 
-class Graph:
-    vertices={}
-    def add_vertex(self, vertex):
-        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
-            self.vertices[vertex.name]=vertex
-            return True
-        else:
-            return False
-    def add_edge(self, u, v):
-        if u in self.vertices and v in self.vertices:
-            for key, value in self.vertices.items():
-                if key==u:
-                    value.add_neighbor(v)
-                if key==v:
-                    value.add_neighbor(u)
-            return True
-        else:
-            return False
-    def print_graph(self):
-        for key in self.vertices.keys():
-            print(key, str(self.vertices[key].neighbors))
-
-    def bfs(self, vert):
-        queue = list()
-        vert.distance=0
-        vert.visited=True
-        for v in vert.neighbors:
-            #direct neighbors of vert will have distance of 1 and be added to the queue
-                self.vertices[v].distance=vert.distance+1
-                queue.append(v)
-            
-        while len(queue)>0:
-            u = queue.pop(0)
-            node_u = self.vertices[u]
-
-            node_u.visited=True
-
-            for v in node_u.neighbors:
-                node_v=self.vertices[v]
-                if node_v.visited==False:
-                    queue.append(v)
-                    if node_v.distance > node_u.distance + 1:
-                        node_v.distance=node_u.distance+1
-            
-g = Graph()
-a = Vertex("A")
-g.add_vertex(a)
-g.add_vertex(Vertex("B"))
-letters = ["C", "D", "E", "F", "G", "H"]
-for i in letters:
-    g.add_vertex(Vertex(i))
-
-edges = ["AB", "AE", "BE", "BF", "CG", "DE", "DH", "EF", "FG", "FH", "FJ", "GJ", "HI"]
-for edge in edges:
-    g.add_edge(edge[:1], edge[1:])
-farts = [['P', 'A', 'H', 'N'], ['A', 'P', 'L', 'S', 'I', 'I', 'G'], ['Y', 'I', 'R']]
+# Python program for Dijkstra's single
+# source shortest path algorithm. The program is
+# for adjacency matrix representation of the graph
+ 
+# Library for INT_MAX
+import sys
+ 
+ 
+class Graph():
+ 
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+ 
+    def printSolution(self, dist):
+        print("Vertex \tDistance from Source")
+        for node in range(self.V):
+            print(node, "\t", dist[node])
+ 
+    # A utility function to find the vertex with
+    # minimum distance value, from the set of vertices
+    # not yet included in shortest path tree
+    def minDistance(self, dist, sptSet):
+ 
+        # Initialize minimum distance for next node
+        min = sys.maxsize
+ 
+        # Search not nearest vertex not in the
+        # shortest path tree
+        for u in range(self.V):
+            if dist[u] < min and sptSet[u] == False:
+                min = dist[u]
+                min_index = u
+ 
+        return min_index
+ 
+    # Function that implements Dijkstra's single source
+    # shortest path algorithm for a graph represented
+    # using adjacency matrix representation
+    def dijkstra(self, src):
+ 
+        dist = [sys.maxsize] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+ 
+        for cout in range(self.V):
+ 
+            # Pick the minimum distance vertex from
+            # the set of vertices not yet processed.
+            # x is always equal to src in first iteration
+            x = self.minDistance(dist, sptSet)
+ 
+            # Put the minimum distance vertex in the
+            # shortest path tree
+            sptSet[x] = True
+ 
+            # Update dist value of the adjacent vertices
+            # of the picked vertex only if the current
+            # distance is greater than new distance and
+            # the vertex in not in the shortest path tree
+            for y in range(self.V):
+                if self.graph[x][y] > 0 and sptSet[y] == False and \
+                        dist[y] > dist[x] + self.graph[x][y]:
+                    dist[y] = dist[x] + self.graph[x][y]
+ 
+        self.printSolution(dist)
+ 
+ 
+# Driver's code
+if __name__ == "__main__":
+    g = Graph(9)
+    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+               [4, 0, 8, 0, 0, 0, 0, 11, 0],
+               [0, 8, 0, 7, 0, 4, 0, 0, 2],
+               [0, 0, 7, 0, 9, 14, 0, 0, 0],
+               [0, 0, 0, 9, 0, 10, 0, 0, 0],
+               [0, 0, 4, 14, 10, 0, 2, 0, 0],
+               [0, 0, 0, 0, 0, 2, 0, 1, 6],
+               [8, 11, 0, 0, 0, 0, 1, 0, 7],
+               [0, 0, 2, 0, 0, 0, 6, 7, 0]
+               ]
+ 
+    g.dijkstra(0)
+ 
